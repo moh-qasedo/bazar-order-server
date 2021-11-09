28746d6627aa886622b9dae652b093ef8fbd25b6
Mohammed Qasedo <qasedo2000@gmail.com>
2021-11-10 00:23:19 +0200

    add the required packages and build REST APIs

--- index.js
+++ index.js
@@ -11,29 +11,23 @@ app.get("/", (req, res) => {
 
 app.post("/order/purchase/:id", (req, res) => {
   axios
-    .get(`http://192.168.1.114:3000/catalog/info/${req.params.id}`)
+    .get(`http://192.168.255.148:3000/catalog/info/${req.params.id}`)
     .then((response) => {
       if (response.data.quantity > 0) {
         axios
-          .put(`http://192.168.1.114:3000/catalog/update/${req.params.id}`)
+          .put(`http://192.168.255.148:3000/catalog/update/${req.params.id}`)
           .then((updateResponse) => {
             res.status(200).send(`Bought book ${response.data.title}`);
           })
-          .catch((error) =>
-            res
-              .status(JSON.parse(JSON.stringify(error)).status)
-              .send("Something went wrong while updating book quantity")
-          );
+          .catch((error) => res.status(403).send(error.response.data));
       } else {
         res
-          .status(410)
+          .status(403)
           .send(`This "${response.data.title}" book is out of stock`);
       }
     })
     .catch((error) => {
-      res
-        .status(JSON.parse(JSON.stringify(error)).status)
-        .send(`Book with id: ${req.params.id} Not Found`);
+      res.status(403).send(error.response.data);
     });
 });
 app.post;
